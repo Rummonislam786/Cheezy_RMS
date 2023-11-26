ee2474446d7892bd8ab51a055985003c425719b6
islam <islam@UPOMA-PC>
2023-11-26 22:29:38 +0600

    v1

---
+++ src/bubt_rms/SqlConn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bubt_rms;
+
+/**
+ *
+ * @author islam
+ */
+public class SqlConn {
+    public static String sqlConnection = "jdbc:mysql://localhost/cheezyrms_bubt";
+    public static String sqlUser = "root";
+    public static String sqlPass = "";
+    
+}
